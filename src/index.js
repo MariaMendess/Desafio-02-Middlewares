404058b2847fa79985621096c228a9d294da5b44
MariaMendess <mariaodetetenorio@gmail.com>
2021-12-23 21:52:34 -0300

    coding: Codificaçao dos Middlewares

--- src/index.js
+++ src/index.js
@@ -10,19 +10,59 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find(user => user.username === username);
+  if (!user) {
+    return response.status(404).json({ error: "User not Exists!" })
+  }
+
+  request.user = user
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+  const todos = user.todos.length;
+
+  if (user.pro === true || todos < 9) {
+    return next()
+  }
+
+  return response.status(403).json({ error: "User don't have permission!" })
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+  if (user) {
+    if (validate(id) === true) {
+      const todo = user.todos.find(todo => todo.id === id);
+      if(todo) {
+        request.user = user;
+        request.todo = todo;
+  
+        return next()
+
+      } else { return response.status(404).json({ error: "Todo not Exists!" }) }
+    } else { return response.status(400).json({ error: "Id invalid!" }) }
+  } else { return response.status(404).json({ error: "User not Exists!" }) }
+
+  
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const user = users.find(user => user.id === id);
+  if (!user) {
+    return response.status(404).json({ error: "User not Exists!" })
+  }
+
+  request.user = user
+  return next()
 }
 
 app.post('/users', (request, response) => {
